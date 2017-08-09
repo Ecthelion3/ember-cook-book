d4eba059e804eae67ca0286ca60df62794582584
Ecthelion3 <matthijs.vandenberg@gmail.com>
2017-08-09 13:58:49 +0200

    add ingredients to recipe

---
+++ app/routes/recipes/actions.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model(params) {
+    return this.get('store').findRecord('recipe', params.recipe_id);
+  },
+  actions: {
+    save(){
+      this.currentModel.save().then(() => this.transitionTo('recipes'));
+    },
+
+    willTransition() {
+      this.controller.get('model').rollbackAttributes();
+    },
+    addIngredient() {
+      this.currentModel.get('ingredients').pushObject(
+        this.get('store').createRecord('ingredient')
+      );
+    }
+  }
+});
